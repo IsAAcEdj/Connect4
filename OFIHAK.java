8764a5abc2c6eded1678551aae804450244d8517
IsAAcEdj <xxxrakexxx@gmail.com>
2025-03-13 23:34:19 -0400

    Add files via upload

---
+++ OFIHAK.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OFIHAK {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String input, pat, gob = "";
+        System.out.println("Enter some text");
+        input = keyboard.nextLine();
+        for (int i = 0; i < input.length(); i =i++) {
+            pat = input(0, i);
+            for
+        System.out.println(gob);
+
+    }
+  }
